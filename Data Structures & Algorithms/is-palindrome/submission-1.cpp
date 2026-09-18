212b11a26f1877a5e8268e025ffb74e9d1b981b8
EL HANI Mohammed- Rida <114149100+HaniMo137@users.noreply.github.com>
2026-09-18 19:04:17 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        for (int j = 0; j < s.size(); j++){
+            if (isalnum(s[j])){
+                s[i] = tolower(s[j]);
+                i++;
+            }
+        }
+        s.resize(i);
+        for (int j = 0; j < i/2; j++){
+            if (s[j] != s[i-j-1])
+               return false;
+        }
+        return true;
+    }
+};
